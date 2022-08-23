db71e93d3a3a36777fd7738b9a48bd110a0ffb53
Lorenzo Bartolini <lori.barto@live.it>
2022-08-23 12:44:30 +0200

    make a working react prototype

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const playSound = () => {
+  const player = new Audio('./kick.wav');
+  player.play();
 }
+
+const App = () =>
+
+  <div className="App">
+    <div className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Hello world!
+      </p>
+      <main>
+        <button onClick={playSound}>1</button>
+        <button>2</button>
+        <button>3</button>
+      </main>
+    </div>
+  </div >
 
 export default App;
